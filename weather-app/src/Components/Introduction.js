e84a36f9b08d0bd0cae8efc052cadf301444b247
Mmanyuu <omanyu91@gmail.com>
2024-10-06 19:58:03 +0800

    Added all the neccessary folders and initial files

---
+++ weather-app/src/Components/Introduction.js
@@ -0,0 +1,5 @@
+//Sorry bindu, i started this cause i think this might be the first main page to be inside the app. hence i exported it first and placed in App.js
+
+function Introduction () {}
+
+export default Introduction;
