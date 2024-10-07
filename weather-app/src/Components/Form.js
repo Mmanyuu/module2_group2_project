ee06e9a05f93424ce855e56e0e726cd8511d5054
cloudi32003 <cloudi32003@gmail.com>
2024-10-07 22:56:29 +0800

    Create New Branch called Form

---
+++ weather-app/src/Components/Form.js
@@ -0,0 +1,16 @@
+// Form to Read, Add, Update and Delete user dummy data (UsersData.js)
+// Letting user to add other activity on current day
+// https://www.atlassian.com/git/tutorials/comparing-workflows/feature-branch-workflow
+
+const EditForm = () => {
+
+
+    return (
+        <>
+        <h1>Edit Details</h1>
+
+        </>
+    )
+}
+
+export default EditForm;
