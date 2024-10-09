4fd01a430a4f413eb4ad872aa92ad93a257ea18e
Mmanyuu <omanyu91@gmail.com>
2024-10-09 14:26:00 +0800

    Updated css for form, weatherdata and header

---
+++ weather-app/src/Components/ViewWeatherData.js
@@ -0,0 +1,33 @@
+// import styles from "./WeatherData.module.css";
+
+// function ViewWeatherData() {
+
+// return (
+//   <div className={styles.container}>
+//     <header>
+//       <h2>Today in {capitalizeFirstLetter(location)}, it's</h2>
+//     </header>
+
+//     <div>
+//       <h1>
+//         ☀ {Math.round(weatherData.main.temp)}
+//         <span class={styles.degreeCelcius}>°C</span> |{" "}
+//         {weatherData.weather[0].description}
+//       </h1>
+//       {/* <p>Feels like: {Math.round(weatherData.main.feels_like)}°C</p> */}
+//     </div>
+
+//     <div className={styles.flexRow}>
+//       <div>Humidity: {weatherData.main.humidity}%</div>
+//       <div>Wind Speed: {weatherData.wind.speed} m/s</div>
+//       <div>
+//         Rainfall in last hour: {weatherData.rain ? weatherData.rain["1h"] : 0}{" "}
+//         mm
+//       </div>
+//       <div>Cloudiness: {weatherData.clouds.all}%</div>
+//       {/* <p>Pressure: {weatherData.main.pressure} hPa</p> */}
+//     </div>
+//   </div>
+// );
+// };
+// export default ViewWeatherData;
