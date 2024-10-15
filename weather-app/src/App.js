e1103d895bcb33b75f4c449176a392d117f212ee
Mmanyuu <omanyu91@gmail.com>
2024-10-15 20:43:45 +0800

    Added link to four day forecast

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,6 +4,7 @@ import { ResponsesProvider } from "./Context/ResponsesContext";
 import Introduction from "./Components/Introduction";
 import MainPage from "./Components/MainPage";
 import RedirectOnRefresh from "./Components/RedirectOnRefresh";
+import FourDayForecastNew from "./Components/FourDayForecastNew";
 
 function App() {
   return (
@@ -13,7 +14,12 @@ function App() {
           <RedirectOnRefresh>
             <Routes>
               <Route path="/" element={<Introduction />} />{" "}
-              <Route path="/MainPage" element={<MainPage />} />
+              <Route path="/MainPage" element={<MainPage />}>
+                <Route
+                  path="/MainPage/FourDayForecastNew"
+                  element={<FourDayForecastNew />}
+                />{" "}
+              </Route>
             </Routes>
           </RedirectOnRefresh>
         </Router>
