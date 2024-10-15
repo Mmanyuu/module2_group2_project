64b10c34771b662e96d8d8636a7810b61f01b182
Mmanyuu <omanyu91@gmail.com>
2024-10-15 20:45:20 +0800

    adjustment to visual

--- weather-app/src/Components/PersonalisedInfo.js
+++ weather-app/src/Components/PersonalisedInfo.js
@@ -103,9 +103,86 @@ function PersonalisedInfo() {
             </div>
 
             <div>
-              <h2>Hello, {latestUser.name},</h2>
+              <h2 className={styles.userName}>Hello, {latestUser.name},</h2>
               <p className={styles.quotes}>{`{ ${randomQuote} }`}</p>
+            </div>
 
+            <div className={styles.flexContainer}>
+              {loading ? (
+                <p>Loading weather data...</p>
+              ) : (
+                <>
+                  {/* Display home weather data */}
+                  {personaliseData.homeWeather ? (
+                    <div className={styles.homeContainer}>
+                      <h4>Home Weather: {latestUser.homeLocation}</h4>
+                      <p>
+                        Weather:{" "}
+                        {personaliseData.homeWeather.weather[0].description}
+                      </p>
+                      <p>
+                        Temperature:{" "}
+                        {Math.round(personaliseData.homeWeather.main.temp)}
+                        °C
+                      </p>
+                      <p>
+                        Feels like:{" "}
+                        {Math.round(
+                          personaliseData.homeWeather.main.feels_like
+                        )}
+                        °C
+                      </p>
+                      <p>
+                        Humidity: {personaliseData.homeWeather.main.humidity}%
+                      </p>
+                      <p>
+                        Wind Speed: {personaliseData.homeWeather.wind.speed} m/s
+                      </p>
+                    </div>
+                  ) : (
+                    <p>
+                      Could not fetch weather data for {latestUser.homeLocation}
+                      .
+                    </p>
+                  )}
+
+                  {/* Display work weather data */}
+                  {personaliseData.workWeather ? (
+                    <div className={styles.workContainer}>
+                      <h4>Work Weather: {latestUser.workLocation}</h4>
+                      <p>
+                        Weather:{" "}
+                        {personaliseData.workWeather.weather[0].description}
+                      </p>
+                      <p>
+                        Temperature:{" "}
+                        {Math.round(personaliseData.workWeather.main.temp)}
+                        °C
+                      </p>
+                      <p>
+                        Feels like:{" "}
+                        {Math.round(
+                          personaliseData.workWeather.main.feels_like
+                        )}
+                        °C
+                      </p>
+                      <p>
+                        Humidity: {personaliseData.workWeather.main.humidity}%
+                      </p>
+                      <p>
+                        Wind Speed: {personaliseData.workWeather.wind.speed} m/s
+                      </p>
+                    </div>
+                  ) : (
+                    <p>
+                      Could not fetch weather data for {latestUser.workLocation}
+                      .
+                    </p>
+                  )}
+                </>
+              )}
+            </div>
+            <div className={styles.activities}>
               {/* -------------------------------------------------------------------------------------------------------------- */}
               {/* this is added for integrate testing purpose. CAn rephrase and render it based on the plannedActivity values 'yes' or 'no'*/}
 
@@ -120,74 +197,6 @@ function PersonalisedInfo() {
 
               {/* ------------------------------------------------------------------------------------------------------------------ */}
             </div>
-
-            {loading ? (
-              <p>Loading weather data...</p>
-            ) : (
-              <>
-                {/* Display home weather data */}
-                {personaliseData.homeWeather ? (
-                  <div className={styles.homeContainer}>
-                    <h4>Home Weather: {latestUser.homeLocation}</h4>
-                    <p>
-                      Weather:{" "}
-                      {personaliseData.homeWeather.weather[0].description}
-                    </p>
-                    <p>
-                      Temperature:{" "}
-                      {Math.round(personaliseData.homeWeather.main.temp)}
-                      °C
-                    </p>
-                    <p>
-                      Feels like:{" "}
-                      {Math.round(personaliseData.homeWeather.main.feels_like)}
-                      °C
-                    </p>
-                    <p>
-                      Humidity: {personaliseData.homeWeather.main.humidity}%
-                    </p>
-                    <p>
-                      Wind Speed: {personaliseData.homeWeather.wind.speed} m/s
-                    </p>
-                  </div>
-                ) : (
-                  <p>
-                    Could not fetch weather data for {latestUser.homeLocation}.
-                  </p>
-                )}
-
-                {/* Display work weather data */}
-                {personaliseData.workWeather ? (
-                  <div className={styles.workContainer}>
-                    <h4>Work Weather: {latestUser.workLocation}</h4>
-                    <p>
-                      Weather:{" "}
-                      {personaliseData.workWeather.weather[0].description}
-                    </p>
-                    <p>
-                      Temperature:{" "}
-                      {Math.round(personaliseData.workWeather.main.temp)}
-                      °C
-                    </p>
-                    <p>
-                      Feels like:{" "}
-                      {Math.round(personaliseData.workWeather.main.feels_like)}
-                      °C
-                    </p>
-                    <p>
-                      Humidity: {personaliseData.workWeather.main.humidity}%
-                    </p>
-                    <p>
-                      Wind Speed: {personaliseData.workWeather.wind.speed} m/s
-                    </p>
-                  </div>
-                ) : (
-                  <p>
-                    Could not fetch weather data for {latestUser.workLocation}.
-                  </p>
-                )}
-              </>
-            )}
           </>
         ) : (
           <p>No users added yet</p>
