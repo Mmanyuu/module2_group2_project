a73407e19230b4e15f8f2d5f553cca01bf16a89d
Mmanyuu <omanyu91@gmail.com>
2024-10-16 16:41:27 +0800

    styling amended for four day forecast

--- weather-app/src/Components/FourDayForecastNew.js
+++ weather-app/src/Components/FourDayForecastNew.js
@@ -1,36 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./FourDayForecastNew.module.css";
-
-//import svg log as react component
-import { ReactComponent as ClearDay } from "./images/clear-day.svg";
-import { ReactComponent as Cloudy } from "./images/cloudy.svg";
-import { ReactComponent as Rainy } from "./images/rain.svg";
-import { ReactComponent as ThunderStormDay } from "./images/thunderstorms-rain.svg";
-import { ReactComponent as Thunder } from "./images/thunderstorms.svg";
-
-// Function to show the correct weather icon based on forecast text
-const getWeatherIcon = (forecastText) => {
-  const lowerCaseText = forecastText.trim().toLowerCase();
-
-  // detecting forecast text to determine what image to use.
-  if (lowerCaseText.includes("fair") || lowerCaseText.includes("clear")) {
-    return <ClearDay className={styles.weatherIcon} />;
-  } else if (
-    lowerCaseText.includes("partly cloudy") ||
-    lowerCaseText.includes("cloudy")
-  ) {
-    return <Cloudy className={styles.weatherIcon} />;
-  } else if (lowerCaseText.includes("rain")) {
-    return <Rainy className={styles.weatherIcon} />;
-  } else if (lowerCaseText.includes("thundery")) {
-    return <ThunderStormDay className={styles.weatherIcon} />;
-  } else if (lowerCaseText.includes("thunder")) {
-    return <Thunder className={styles.weatherIcon} />;
-  } else {
-    return <Cloudy className={styles.weatherIcon} />; // Default icon
-  }
-};
+import WeatherIcon from "./WeatherIcon";
 
 // Four Day Forecast is an API from data.gov.sg, it does not require API key and it does not need any input othan than location, hency the useEffect hook has empty dependency array.
 const FourDayForecastNew = () => {
@@ -71,18 +42,19 @@ const FourDayForecastNew = () => {
 
   return (
     <div>
-      <h3>Four Day Weather Forecast</h3>
       <div className={styles.forecastRow}>
         {forecastData.map((forecast, index) => (
           <div key={index} className={styles.forecastItem}>
-            <strong>Date:</strong> {forecast.day} <br />
-            <strong>Low Temperature:</strong> {forecast.temperature.low}°C{" "}
+            <strong>{forecast.day} </strong> <br />
+            <strong>{forecast.forecast.text}</strong> <br />
+            <WeatherIcon forecastText={forecast.forecast.text} />
             <br />
-            <strong>High Temperature:</strong> {forecast.temperature.high}°C{" "}
-            <br />
-            <strong>Forecast:</strong> {forecast.forecast.text}
-            <br />
-            {getWeatherIcon(forecast.forecast.text)}
+            <p className={styles.temperaturePosition}>
+              {forecast.temperature.high}°C
+            </p>
+            <p className={styles.temperaturePosition}>
+              {forecast.temperature.low}°C
+            </p>
           </div>
         ))}
       </div>
